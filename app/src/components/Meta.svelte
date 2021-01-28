2396dfd522bdf6b3c3c4b8c555b88932dff663d3
xaquingv <xocasgv@gmail.com>
2021-01-28 11:42:03 +0100

    Add manifest.json

--- app/src/components/Meta.svelte
+++ app/src/components/Meta.svelte
@@ -17,7 +17,7 @@
 <meta property="og:url" content={url}/>
 <meta property="og:title" content={title}/>
 <meta property="og:description" content={description}/>
-<meta property="og:image" content="img/poster.png" />
+<meta property="og:image" content="img/poster_social.png" />
 <meta property="og:image:width" content="1600" />
 <meta property="og:image:height" content="900" />
 
@@ -25,6 +25,8 @@
 <meta property="twitter:url" content={url}>
 <meta property="twitter:title" content={title}>
 <meta property="twitter:description" content={description}>
-<meta property="twitter:image" content="img/poster.png">
+<meta property="twitter:image" content="img/poster_social.png">
 <link rel="shortcut icon" type="image/x-icon" href="https://images.squarespace-cdn.com/content/v1/5e5b7eebeb83b746b6481a2d/1604671720103-EPJSELDNWVIPCWI63H7G/ke17ZwdGBToddI8pDm48kNCIUUInZz-JelovUC4TKJ9Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpy2nmdcYIRqEQwGN8P7xrL_yE-hs8s0G614KmYuoTh2b5xIcJ7iebeF3-4wt8VfJy4/favicon.ico?format=100w">
+
+<link rel="manifest" href="manifest.json">
 </svelte:head>
