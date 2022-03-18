20ac6f604c8921ff7e95cdc4590c8c17bc8f9a71
xaquingv <xocasgv@gmail.com>
2022-03-18 19:17:18 +0100

    FIX legend

--- app/src/components/common/Legend.svelte
+++ app/src/components/common/Legend.svelte
@@ -1,46 +1,43 @@
 <script>
-export let legendItems;
-
+  export let legendItems;
 </script>
 
 <ul>
-    {#each legendItems as item}
+  {#each legendItems as item}
     <li>
-        <div class='color {item.class}' style='background-image:{item.color}'>
-            <svg width='1.2rem' height='1.2rem' aria-label='Color para identificar {item.label}'>
-                <rect   
-                        fill={item.color}
-                        width='1.2rem'
-                        height='1.2rem'
-                        x=0
-                        y=0
-                />
-            </svg>
-        </div>
-        <p class='label'>{@html item.label}</p>
+      <div class="color {item.class}" style="background-image:{item.color}">
+        <svg
+          width="1.2rem"
+          height="1.2rem"
+          aria-label="Color para identificar {item.label}"
+        >
+          <rect fill={item.color} width="1.2rem" height="1.2rem" x="0" y="0" />
+        </svg>
+      </div>
+      <p class="label">{@html item.label}</p>
     </li>
-    {/each}
+  {/each}
 </ul>
 
 <style>
-    .color {
-        width:1.2rem;
-        height:1.2rem;
-    }
-    li {
-        display: grid;
-        grid-template-columns: 1rem auto;
-        max-width:10rem;
-        float:left;
-        margin-right:1rem;
-        margin-left:4px;
-    }
-    .label {
-        padding-left:.5rem;
-        padding-top:.1rem;
-        margin:0;
-        color:#505050;
-        font-size: .85rem;
-        font-weight: 400;
-    }
+  .color {
+    width: 1.2rem;
+    height: 1.2rem;
+  }
+  li {
+    display: grid;
+    grid-template-columns: 1rem auto;
+    max-width: 10rem;
+    float: left;
+    margin-right: 1rem;
+    margin-left: 4px;
+  }
+  .label {
+    padding-left: 0.5rem;
+    padding-top: 0.1rem;
+    margin: 0;
+    color: #333;
+    font-size: 0.85rem;
+    font-weight: 400;
+  }
 </style>
